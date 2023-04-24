9b63bf72f09644cd86af8634874baaf6f22daffe
AlexSol <solovejaleksej5@gmail.com>
2023-04-24 18:37:05 +0300

    task_1_completed

--- homework_23.04.2023/task_1/Program.cs
+++ homework_23.04.2023/task_1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArray(int length, int leftSide, int rightSide)
+{
+int[] array = new int[length];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+array[i] = random.Next(leftSide, rightSide);
+return array;
+}
+
+void PrintArray(int[] array)
+{
+foreach (int element in array)
+Console.Write($"{element} |");
+Console.WriteLine(" ");
+}
+
+void searchArray(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] % 2 == 0)
+      count++;
+  }
+  Console.WriteLine("Четные числа встречаются " +count+ " раз!");
+}
+
+int [] array = CreateArray(10, 100, 999);
+PrintArray(array);
+searchArray(array);
