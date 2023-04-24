3302350e973db92032a449aa7f94fd8999d71591
AlexSol <solovejaleksej5@gmail.com>
2023-04-24 18:49:18 +0300

    task_2_completed

---
+++ homework_23.04.2023/task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] CreateArray(int length, int leftSide, int rightSide)
+{
+int[] array = new int[length];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+array[i] = random.Next(leftSide, rightSide);
+return array;
+}
+
+void PrintArray(int[] array)
+{
+foreach (int element in array)
+Console.Write($"{element} |");
+Console.WriteLine(" ");
+}
+
+void summArray(int [] array)
+{ 
+  int summ = 0;
+  for(int i = 1; i < array.Length; i = i+2)
+  {
+    summ = summ + array[i];
+  }
+  Console.WriteLine("Сумма элементов на нечетных позициях равна " +summ);
+}
+
+int [] array = CreateArray(8, 1, 10);
+PrintArray(array);
+summArray(array);
