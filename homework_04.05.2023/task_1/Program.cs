6a5e7807d9fffc57c08f1cc62b0d27b50a8086b3
AlexSol <solovejaleksej5@gmail.com>
2023-05-04 10:26:35 +0300

    add homework_04.05.2023 task_1

---
+++ homework_04.05.2023/task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿double[,] CreateArray(int m, int n, int leftSide, int rightSide)
+{
+double[,] array = new double[m, n];
+Random random = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = Math.Round(random.Next(leftSide, rightSide) + random.NextDouble(), 2);
+    }
+}
+return array;
+}
+void PrintArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(CreateArray(5,5, 100 ,120));
